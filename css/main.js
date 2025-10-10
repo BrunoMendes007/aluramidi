2a83010da9543862c19f05d6238a32a7bbf7145d
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-10-10 10:44:31 +0000

    onkeydown

--- css/main.js
+++ css/main.js
@@ -1,3 +1,29 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom')
+function tocaSomPom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play;
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
+
+const listadeTeclas = document.querySelectorAll('.tecla');
+
+for(let contador = 0; contador < listadeTeclas.length; contador ++){
+
+    const tecla = listaTeclas[contador];
+    const instrumento = tecla.classlist[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function(){
+        tocaSomPom(idAudio);
+    }
+    tecla.onkeydown = function (evento){
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classlist.add('ativa');
+        }
+    }
+}
+listadeTeclas[0].onclick = tocaSomPom;
